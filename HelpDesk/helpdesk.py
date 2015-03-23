04a4812e8231e140f4250519e5c5580ac302d0e4
WeiranFang <weiran.fang@hotmail.com>
2015-03-23 13:52:53 -0400

    Add all

--- HelpDesk/helpdesk.py
+++ HelpDesk/helpdesk.py
@@ -1,49 +1,49 @@
 from flask import Flask, render_template, request, redirect, session, g
+from time import gmtime, strftime
 import sqlite3
 
 app = Flask(__name__)
 
-email_addresses = []
+problems = []
+details = []
 
 @app.before_request
 def before_request():
-    g.db = sqlite3.connect("emails.db")
+    g.db = sqlite3.connect("problems.db")
 
 @app.teardown_request
 def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/request')
+def request_controller():
+    return render_template('request.html')
 
-@app.route('/signup', methods = ['POST'])
-def signup():
-    email = request.form['email']
-    g.db.execute("INSERT INTO email_addresses VALUES (?)", [email])
+@app.route('/submit-problem', methods = ['POST'])
+def submit_problem():
+    name = request.form['InputName']
+    email = request.form['InputEmail']
+    problem_title = request.form['InputTitle']
+    description = request.form['InputMessage']
+    usrfile = request.form['InputFile']
+    current_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    g.db.execute("INSERT INTO user_problems(name, email, title, description, file, time) VALUES (?, ?, ?, ?, ?, ?)", [name, email, problem_title, description, usrfile, current_time])
     g.db.commit()
-    return redirect('/')
+    return redirect('/request')
 
-@app.route('/unregister')
-def unregister():
-    # Make sure they've already registered an email address
-    if 'email' not in session:
-        return "You haven't submitted an email!"
-    email = session['email']
-    # Make sure the email was already in the email list
-    if email not in email_addresses:
-        return "That address is not in our list"
-    email_addresses.remove(email)
-    del session['email'] # Remove email from the session dictionary
-    return "We have removed " + email + " from the list"
+@app.route('/problems')
+def problems():
+    problems = g.db.execute("SELECT * FROM user_problems").fetchall()
+    return render_template('problems.html', problems = problems)
 
-@app.route('/emails.html')
-def emails():
-    email_addresses = g.db.execute("SELECT * FROM email_addresses").fetchall()
-    return render_template('emails.html', email_addresses = email_addresses)
+@app.route('/detail/<pid>')
+def detail(pid):
+    details = g.db.execute("SELECT * FROM user_problems WHERE pid = ?", [pid]).fetchall()
+    return render_template('detail.html', details = details)
 
 if __name__ == '__main__':
     app.secret_key = '\xb1\xa7@\xe5a^#b\x9d\x8f\xc6Dzz\xd6\x8f\x03A\xbf\x94\x99\x19`\x01'
+    app.debug = True
     app.run()
 
